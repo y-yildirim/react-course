477b8227722a845be7799e0cdccc81b0fc006381
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2023-08-10 12:27:49 +0300

    Add basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 function App() {
   return (
-    <div className="App">
-     <h1>Up & Running!</h1>
+    <div>
+      <h1>Todos</h1>
+      <div className="card">
+        <h2>Title</h2>
+        <div className="actions">
+          <button className="btn">Delete</button>
+        </div>
+      </div>
     </div>
   );
 }
