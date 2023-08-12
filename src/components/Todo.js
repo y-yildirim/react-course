b5711225b43d3ee62ed37c61de7076996f50ba30
Yunus YILDIRIM <y.yildirim.ce@gmail.com>
2023-08-12 14:55:35 +0300

    Add onClick event handler to Delete button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,14 @@
 function Todo(props) {
+  function handleDelete() {
+    console.log(props.title);
+  }
   return (
     <div className="card">
       <h2>{props.title}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={handleDelete}>
+          Delete
+        </button>
       </div>
     </div>
   );
